f1a5e9e37f0b2dc47df496526722e4b82c9b2656
Gilberto Avalos <avalosagnostic@gmail.com>
2016-03-29 14:01:29 -0600

    Get teams

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.get('/', function(req, res) {
 });
 
 app.get('/auth', function(req, res) {
-  session.destroy(req.session.id);
+  req.session.destroy();
 
   if (req.query.code) {
     request({
