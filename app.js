9aa4c885be4cf14165860c56c974be2d994a5a97
Gilberto Avalos <avalosagnostic@gmail.com>
2016-03-29 12:39:03 -0600

    Add request package and auth method

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 var express = require('express'),
-  app = express();
+  app = express(),
+  request = require('request'),
+  config = {
+    githubUrl: 'https://github.com',
+    clientSecret: '808ec362dbf485cb5ccd3bb34652d2e080e98217',
+    clientId: 'd3d7cbd7dce23dd9de98'
+  };
 
 // Static assets
 app.use(express.static(__dirname + '/public'));
@@ -10,6 +16,24 @@ app.set('view engine', 'jade');
 
 app.get('/', function(req, res) {
   res.render('index');
+});
+
+app.get('/auth', function(req, res) {
+  if (req.query.code) {
+    request({
+      url: config.githubUrl + '/login/oauth/access_token',
+      method: 'POST',
+      qs: {
+        client_id: config.clientId,
+        client_secret: config.clientSecret,
+        code: req.query.code
+      }
+    }, function(error, response, body) {
+      res.end(body);
+    });
+  } else {
+    res.redirect('/');
+  }
 });
 
 app.get('/api', function(req, res) {
