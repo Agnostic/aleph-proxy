c190e06de962c2f5ab62f49bdca4fdebe2b620ef
Gilberto Avalos <avalosagnostic@gmail.com>
2016-03-29 15:51:50 -0600

    Add pr info

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,11 @@
 var app = angular.module('prApp', []);
 
+app.filter('to_trusted', ['$sce', function($sce){
+  return function(text) {
+    return $sce.trustAsHtml(text);
+  };
+}]);
+
 app.controller('pullsController', ['$scope', '$http', function($scope, $http) {
   $http.get('/api/pulls').then(function(response) {
     $scope.pulls = response.data;
