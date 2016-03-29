2d0ba575efdbd738cb055bc9c883f08c922693c8
Gilberto Avalos <avalosagnostic@gmail.com>
2016-03-29 16:27:34 -0600

    Add reviewers

--- public/js/app.js
+++ public/js/app.js
@@ -7,13 +7,26 @@ app.filter('to_trusted', ['$sce', function($sce){
 }]);
 
 app.controller('pullsController', ['$scope', '$http', function($scope, $http) {
+  var approvalKeywords = ['+1', ':+1:', ':gem:', 'lgtm'];
+
   $http.get('/api/pulls').then(function(response) {
     $scope.pulls = response.data;
 
     angular.forEach($scope.pulls, function(pull) {
       $http.get('/api/comments?url=' + pull.comments_url)
         .then(function(response) {
-          pull.comments = response.data;
+          pull.approvedBy = [];
+
+          angular.forEach(response.data, function(comment) {
+            var message = comment.body.replace(/^\s+|\s+$/g, '').toLowerCase();
+            if (_.contains(approvalKeywords, message)) {
+              pull.approvedBy.push({
+                user: comment.user.login,
+                avatar: comment.user.avatar_url,
+                url: comment.user.html_url
+              });
+            }
+          });
         });
     });
   });
