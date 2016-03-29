6025c60cc153e2f5de8c5516cfbdaf5e847acd41
Gilberto Avalos <avalosagnostic@gmail.com>
2016-03-29 15:47:54 -0600

    Add pr info

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,5 @@ var app = angular.module('prApp', []);
 app.controller('pullsController', ['$scope', '$http', function($scope, $http) {
   $http.get('/api/pulls').then(function(response) {
     $scope.pulls = response.data;
-    console.log($scope.pulls);
   });
 }]);
