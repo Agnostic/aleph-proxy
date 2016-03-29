299bc52dba6f6e60d24a96242fb3070c3bd4e80e
Gilberto Avalos <avalosagnostic@gmail.com>
2016-03-29 12:23:05 -0600

    Add jade template engine

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 var express = require('express'),
   app = express();
 
+// Static assets
+app.use(express.static(__dirname + '/public'));
+
+// Template engine
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
 app.get('/', function(req, res) {
+  res.render('index');
+});
+
+app.get('/api', function(req, res) {
   res.end('It works :)');
 });
 
-app.listen(80);
+app.listen(process.env.PORT || 5000);
