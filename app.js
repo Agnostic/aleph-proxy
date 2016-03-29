353acddc18ecec1022944800d49e559f770f66a8
Gilberto Avalos <avalosagnostic@gmail.com>
2016-03-29 13:35:20 -0600

    Get org repos

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.set('view engine', 'jade');
 app.get('/', function(req, res) {
   if (req.session.accessToken) {
     request({
-      url: config.apiUrl + '/orgs/wepow/wepow-app/pulls?access_token=' + req.session.accessToken,
+      url: config.apiUrl + '/orgs/wepow/repos?access_token=' + req.session.accessToken,
       headers: {
         'User-Agent': config.userAgent,
         Accept: 'application/json'
